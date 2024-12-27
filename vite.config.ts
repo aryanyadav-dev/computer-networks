5d7bd6f3b6c194cffc6d138ec50b1eb35224e608
Aryan Yadav <aryanyadav7825@gmail.com>
2024-12-27 13:12:49 +0530

    Deployment fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), 
-    },
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-        },
-      },
-    },
-  },
-  server: {
-    historyApiFallback: true, 
   },
 });
